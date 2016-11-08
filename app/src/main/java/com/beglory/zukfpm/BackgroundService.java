a444d2c67696ff3b5b009c0d4b7b3042de0feef1
Stanislav Petryakov <become.glory@yandex.ru>
2016-11-08 14:43:28 +0300

    Improve last app switching

--- app/src/main/java/com/beglory/zukfpm/BackgroundService.java
+++ app/src/main/java/com/beglory/zukfpm/BackgroundService.java
@@ -7,7 +7,6 @@
 
 package com.beglory.zukfpm;
 
-import android.app.ActivityManager;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -15,8 +14,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -27,8 +26,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import static android.app.ActivityManager.MOVE_TASK_NO_USER_ACTION;
 
 public class BackgroundService extends Service {
     private final static String FPC_HAL = "fpc_fingerprint_hal:D";
@@ -41,7 +38,6 @@ public class BackgroundService extends Service {
     public final static String ACTION_CHANGE = "com.beglory.zukfpm.CHANGE";
 
     private Thread mThread;
-    private ActivityManager mActivityManager;
     private ActionReceiver mScreenReceiver = new ActionReceiver();
     private long mLastCheck;
     private SimpleDateFormat mDateFormat;
@@ -63,7 +59,6 @@ public class BackgroundService extends Service {
 
         readPreferences();
 
-        mActivityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         mDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.getDefault());
     }
 
@@ -124,7 +119,7 @@ public class BackgroundService extends Service {
                         dos.flush();
 
                         String line;
-                        while (mIsRunning && (line = reader.readLine()) != null) {
+                        while (mIsRunning && isScreenOn() && (line = reader.readLine()) != null) {
                             if (line.contains("nav_event_report")) {
                                 long time = mDateFormat.parse("2016-" + line.substring(0, 18)).getTime();
                                 if (time > mLastCheck) {
@@ -141,29 +136,38 @@ public class BackgroundService extends Service {
                                     if (finalLine.equals(mLastKey) && mLast) {
                                         Process activities = Runtime.getRuntime().exec("su");
                                         DataOutputStream dos2 = new DataOutputStream(activities.getOutputStream());
-//                                        dos2.writeBytes("dumpsys activity activities\n");
                                         dos2.writeBytes("dumpsys activity recents\n");
                                         dos2.flush();
                                         BufferedReader reader2 = new BufferedReader(new InputStreamReader(activities.getInputStream()));
 
-                                        List<Integer> list = new ArrayList<>();
+                                        List<LastApp> list = new ArrayList<>();
                                         while ((line = reader2.readLine()) != null) {
-//                                            if (line.contains("Run #")) {
                                             if (line.contains("Recent #")) {
                                                 if (line.contains(mLauncherPackage))
                                                     if (list.size() != 0)
                                                         continue;
 
-                                                list.add(parseTaskId(line));
+                                                int taskId = parseTaskId(line);
+                                                String affinity = parseAffinity(line);
+                                                list.add(new LastApp(affinity, taskId));
+                                            } else if (line.contains("realActivity")) {
+                                                if (list.size() > 0) {
+                                                    String affinity = list.get(list.size() - 1).getAffinity();
+                                                    if (line.contains(affinity))
+                                                        list.get(list.size() - 1).setActivity(parseActivity(line));
+                                                }
+
                                                 if (list.size() == 2)
                                                     break;
-//                                            } else if (line.contains("mFocusedActivity"))
                                             } else if (line.contains("hasBeenVisible=false"))
                                                 break;
                                         }
 
-                                        if (list.size() > 1)
-                                            mActivityManager.moveTaskToFront(list.get(1), MOVE_TASK_NO_USER_ACTION);
+                                        if (list.size() > 1) {
+                                            dos2.writeBytes("am start " + list.get(1).getActivity() + "\n");
+                                            dos2.flush();
+                                            dos2.close();
+                                        }
                                     }
 
                                     mLastCheck = time;
@@ -183,16 +187,49 @@ public class BackgroundService extends Service {
 
     private int parseTaskId(String line) {
         String[] elements = line.split(" ");
-//        String id = elements[elements.length - 1];
-//        id = id.substring(1, id.length() - 1);
         String id = elements[6];
         id = id.substring(1);
         return Integer.parseInt(id);
     }
 
-    @Nullable
+    private String parseAffinity(String line) {
+        String[] elements = line.split(" ");
+        return elements[7].substring(2);
+    }
+
+    private String parseActivity(String line) {
+        return line.replace("    realActivity=", "");
+    }
+
+    public boolean isScreenOn() {
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        return powerManager.isInteractive();
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private class LastApp {
+        private String mAffinity, mActivity;
+        private int mTaskId;
+
+        LastApp(String affinity, int taskId) {
+            mAffinity = affinity;
+            mTaskId = taskId;
+        }
+
+        void setActivity(String activity) {
+            mActivity = activity;
+        }
+
+        String getAffinity() {
+            return mAffinity;
+        }
+
+        String getActivity() {
+            return mActivity;
+        }
     }
 }
